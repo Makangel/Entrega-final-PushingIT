64980e69bc0bb6fd8c8cc512e7b13064dff783a3
Asgro <sgroangie@gmail.com>
2023-03-21 12:03:53 -0300

    [primer push no se a donde]

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    "baseUrl" : 'https://pushing-front.vercel.app/',
+    watchForFileChanges: false,
+    //defaultCommandTimeout: 10000,
+  },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: true,
+    html: true,
+    json: true,
+  },
+});
